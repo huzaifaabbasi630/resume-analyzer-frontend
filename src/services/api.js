c74ef199edf0804903674b789e616c2207200ffa
huzaifaabbasi630 <huzaifaabbasi09123@gmail.com>
2026-05-22 17:37:35 +0500

    add backend link

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://analyzer-backend-hig5jyn6x-huzaifas-projects-84375059.vercel.app/api',
 });
 
 // Request Interceptor
